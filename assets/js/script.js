c4b6670ffc32638b81ad46c7f63425b988b4b537
ELSON-341 <elson.sn314@gmail.com>
2024-04-15 10:43:26 -0300

    slide de banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 'use strict'
-
+// Elementos
 const cartContainer = document.querySelector('#cart-container')
+const loginForm = document.querySelector('#login-form')
 
+// Eventos
 document.querySelector('#cart').onclick = () => {
     cartContainer.classList.toggle('active')
+
+    loginForm.classList.remove('active')
+}
+
+document.querySelector('#login').onclick = () => {
+    loginForm.classList.toggle('active')
+
+    cartContainer.classList.remove('active')
 }
