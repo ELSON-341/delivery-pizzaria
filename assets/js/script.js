01ff387dddf4996d873ef7c3fa24585e2770ba7d
ELSON-341 <elson.sn314@gmail.com>
2024-04-28 09:53:01 -0300

    fim home

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,27 @@
 // Elementos
 const cartContainer = document.querySelector('#cart-container')
 const loginForm = document.querySelector('#login-form')
+const menuContainer = document.querySelector('header nav')
+console.log(menu);
+console.log(loginForm);
 
 // Eventos
 document.querySelector('#cart').onclick = () => {
     cartContainer.classList.toggle('active')
-
     loginForm.classList.remove('active')
+    menuContainer.classList.toggle('active')
 }
 
 document.querySelector('#login').onclick = () => {
     loginForm.classList.toggle('active')
-
     cartContainer.classList.remove('active')
+    menuContainer.classList.remove('active')
+}
+
+document.querySelector('#menu').onclick = () => {
+    menuContainer.classList.toggle('active')
+    cartContainer.classList.remove('active')
+    loginForm.classList.remove('active')
 }
 
 // slide with CDN swiper
